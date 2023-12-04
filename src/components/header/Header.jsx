088ef082b4bd85b2f605ac05a828fc7ccca7f79d
sbrycbc <sabriyecbc@gmail.com>
2023-12-04 15:34:11 +0100

    added Header icon and avatar...

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Logo from '../logo/Logo';
 import FindReplaceIcon from '@mui/icons-material/FindReplace';
+import HomeIcon from '@mui/icons-material/Home';
+import ModeCommentIcon from '@mui/icons-material/ModeComment';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import { Avatar } from '@mui/material';
+import AvatarFoto from '../../assets/images/sbry.jpg'
 import "./header.css";
 
 const Header = () => {
@@ -12,6 +17,22 @@ const Header = () => {
             <div className="search">
               <FindReplaceIcon className='search-icon'/>
               <input type="text" placeholder='Search' />
+              </div>
+              <div className="header-links">
+                <a href="/">
+                   <HomeIcon className='icon'/>
+                </a>
+                <a href="/">
+                   <ModeCommentIcon className='icon'/>
+                </a>
+                <a href="/">
+                   <AddBoxIcon className='icon'/>
+                </a>
+                <a href="/">
+                <Avatar alt="Cindy Baker"
+                src={AvatarFoto}
+                sx={{ width: 28, height: 28 }} />
+                </a>
               </div>
             </div>
         </div>      
