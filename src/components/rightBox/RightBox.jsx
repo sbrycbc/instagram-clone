997ef766ec528e0e9482e46133e2961123b9f765
sbrycbc <sabriyecbc@gmail.com>
2023-12-05 13:42:35 +0100

    updated rightBox with styling...

--- src/components/rightBox/RightBox.jsx
+++ src/components/rightBox/RightBox.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
-import './rightBox.css';
+import React from "react";
+import { Avatar } from "@mui/material";
+import { Button } from "@mui/material";
+import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
+
+import AvatarPhoto from "../../assets/images/sbry.jpg";
+import FriendPhoto from "../../assets/images/blog2.jpg";
+import "./rightBox.css";
 
 const RightBox = () => {
   return (
-    <div>RightBox</div>
-  )
-}
+    <div className="right-box">
+      <div className="account">
+        <a href="/">
+          <Avatar src={AvatarPhoto} sx={{ width: 56, height: 56 }} />
+        </a>
+        <div className="account-titles">
+          <a href="/">sabriyecebeci</a>
+          <span>Sabriye Cebeci</span>
+        </div >
+      </div>
+      <span className="friends-title">My Friends</span>
+      <ul className="friends-list">
+        <li className="friend-item">
+          <div className="friend-item-left">
+            <a href="/">
+              <Avatar src={FriendPhoto} sx={{ width: 40, height: 40 }} />
+            </a>
+            <div className="friend-username">
+              <a href="/">gölgemsi</a>
+              <span>Gölge Hanim</span>
+            </div>
+          </div>
+          <div className="friend-item-right">
+            <Button
+              variant="contained"
+              endIcon={<ArrowCircleRightOutlinedIcon />}
+              size="small"
+            >
+              Contained
+            </Button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default RightBox
+export default RightBox;
