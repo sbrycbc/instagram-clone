3b4b1908377aba527fb0a90bf73bd8fffacb16f0
sbrycbc <sabriyecbc@gmail.com>
2023-12-14 14:59:33 +0100

    added login router...

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 
-//register
+// register
 router.post("/register", async(req, res)=> {
     try{
         const {fullName, username, email, password, bio, profilePicture} = req.body;
@@ -26,6 +26,28 @@ router.post("/register", async(req, res)=> {
     res.status(500).json(err);
   }
 });
+
+// login
+router.post("/login", async (req, res) => {
+    try {
+      const user = await User.findOne({ email: req.body.email });
+      !user && res.status(404).send("User not found!");
+  
+      const validPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+  
+      if (!validPassword) {
+        res.status(403).send("Invalid password!");
+      } else {
+        res.status(200).json(user);
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 
 
 module.exports = router
