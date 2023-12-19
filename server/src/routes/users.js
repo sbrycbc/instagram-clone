f1f21d3268de2ee0dca063b19f1b6d185986019f
sbrycbc <sabriyecbc@gmail.com>
2023-12-19 12:59:37 +0100

    added delete user

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -25,4 +25,18 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+// delete user
+
+router.delete("/:id", async (req, res) => {
+  if ((req.body.userId = req.params.id || req.body.isAdmin)) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Account has been deleted!!!")
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  }
+});
+
+
 module.exports = router;
