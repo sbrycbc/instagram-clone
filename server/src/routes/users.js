ccd00ef4b9c5cf52e8e96376f619c878fdb40d30
sbrycbc <sabriyecbc@gmail.com>
2023-12-19 14:34:43 +0100

    added follow and unfollow users...

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -65,4 +65,69 @@ router.get("/list", async (req, res) => {
     }
 });
 
+// follow a user
+
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.params.id } });
+        res.status(200).json("User has been followed!");
+      } else {
+        res.status(403).send("You are not following this user!");
+      }
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  } else {
+    res.status(403).send("You can't follow yourself!");
+  }
+});
+
+// unfollow a user
+
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { followings: req.params.id } });
+        res.status(200).json("User has been unfollowed!");
+      } else {
+        res.status(403).send("You are not unfollowing this user");
+      }
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  } else {
+    res.status(403).send("You can't unfollow yourself!");
+  }
+});
+
+// get friends
+
+router.get("/friends/:userId", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    const friends = await Promise.all(
+      user.followings.map((friendId) => {
+        return User.findById(friendId);
+      })
+    );
+    let friendList = [];
+    friends.map((friend) => {
+      const { _id, username, fullName, profilePicture } = friend;
+      friendList.push({ _id, username, fullName, profilePicture });
+    });
+    res.status(200).json(friendList);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
